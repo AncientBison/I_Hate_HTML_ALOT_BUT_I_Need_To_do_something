b8b5d56ac351dae9d52395d646b3add523c0379c
Ancient Bison <ilansbernstein@gmail.com>
2021-01-02 00:44:24 +0000

    alot

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-function type(classname) {
+function type(classname) { //types a charecter through out one board.
   a = document.getElementsByClassName(classname);
   for (var i = 0; i < a.length; i++) {
     a[i].innerText = "?";
   }
 }
-type("p");
-type("o");
+
+type("pl");
+type("ol");
 
 o = "o"
 p = "p"
 input = ""
 code = "fghijklmnopqrstuvwxz"
+op_word = "abcde"
 
-function setCharecter(board, number, letter) {
+function setCharecter(board, number, letter) { //when opponent takes a letter 
   console.log(board + JSON.stringify(number))
-  document.getElementById(board + JSON.stringify(number)).innerText = letter;
+  document.getElementById(board +  "l" + JSON.stringify(number)).innerText = letter;
 }
 
-function askForLetter(td) {
-  td.innerText = code.split("")[td.id.split("o")[1]-6] == "x" ? "xy" : code.split("")[td.id.split("o")[1]-6]
+function askForLetter(td) { //gets a opponets letter based on a twenety charecter code
+  td.querySelector(".ol").style.backgroundColor = "black"
+  td.querySelector(".ol").innerText = code.split("")[td.id.split("o")[1]-6] == "x" ? "xy" : code.split("")[td.id.split("o")[1]-6]
+}
+
+function say(message) { //shows a overlay message
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlaytext").innerText = message
+}
+
+function closeOverlay() { //closes the overlay
+  document.getElementById("overlay").style.display = "none";
+}
+
+function finishGame() { //changes top letters based on opponent word
+  document.getElementById("o1").querySelector(".ol").style.backgroundColor = "black"
+  document.getElementById("o1").querySelector(".ol").innerText = op_word.split("")[document.getElementById("o1").id.split("o")[1] - 1]
+  document.getElementById("o2").querySelector(".ol").style.backgroundColor = "black"
+  document.getElementById("o2").querySelector(".ol").innerText = op_word.split("")[document.getElementById("o2").id.split("o")[1] - 1]
+  document.getElementById("o3").querySelector(".ol").style.backgroundColor = "black"
+  document.getElementById("o3").querySelector(".ol").innerText = op_word.split("")[document.getElementById("o3").id.split("o")[1] - 1]
+  document.getElementById("o4").querySelector(".ol").style.backgroundColor = "black"
+  document.getElementById("o4").querySelector(".ol").innerText = op_word.split("")[document.getElementById("o4").id.split("o")[1] - 1]
+  document.getElementById("o5").querySelector(".ol").style.backgroundColor = "black"
+  document.getElementById("o5").querySelector(".ol").innerText = op_word.split("")[document.getElementById("o5").id.split("o")[1] - 1]
 }
